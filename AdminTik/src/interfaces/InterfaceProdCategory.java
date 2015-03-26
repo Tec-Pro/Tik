5f2d45ddd926c347907162fc76404d88e7cc735e
agusbauer <agustinbauer1@gmail.com>
2015-03-26 17:17:08 -0300

    AdminTik client project created

---
+++ AdminTik/src/interfaces/InterfaceProdCategory.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package interfaces;
+
+import java.rmi.Remote;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author nico
+ */
+public interface InterfaceProdCategory extends Remote {
+
+    public Map<String,Object> create(String name) throws java.rmi.RemoteException;
+     
+     public Map<String,Object> modify(int id,String name) throws java.rmi.RemoteException;
+     
+     public boolean delete(int id) throws java.rmi.RemoteException;
+     
+     public Map<String,Object> getProdCategory(int id) throws java.rmi.RemoteException;     
+     
+     public  List<Map> getPCategories() throws java.rmi.RemoteException;    
+}
