921334b19ad80937ab452805e4f98b2d25dd2a52
ezensich <ezensich@gmail.com>
2015-05-19 15:26:29 -0300

    Correccion en interfaceName

--- WaiterTik/src/utils/InterfaceName.java
+++ WaiterTik/src/utils/InterfaceName.java
@@ -5,6 +5,8 @@
  */
 package utils;
 
+import java.rmi.registry.Registry;
+
 /**
  * Nombres de interfaces que provienen desde el server
  *
@@ -25,5 +27,7 @@ public class InterfaceName {
     public static final String CRUDPresence = "CRUDPresence";
     public static final String CRUDOrder = "CRUDOrder";
     public static final String server = "Server";
+    public static Registry registry;
+    public static final String GeneralConfig = "GeneralConfig";
 
 }
