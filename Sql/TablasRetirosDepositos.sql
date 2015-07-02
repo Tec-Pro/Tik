6164e49f9a9157ce0b43340cec5db4f6171f59d6
jheredia <joaquinheredia93@gmail.com>
2015-07-01 22:17:19 -0500

    Modificació tablas retiros depósitos, borrar las tablas viejas

--- Sql/TablasRetirosDepositos.sql
+++ Sql/TablasRetirosDepositos.sql
@@ -1,27 +1,20 @@
-CREATE TABLE `admindeposits` (
-  `id` int(11) NOT NULL,
-  `admin_id` int(11) DEFAULT NULL,
+CREATE TABLE `deposits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `admin_id` int(11) DEFAULT '0',
+  `waiter_id` int(11) DEFAULT '0',
   `amount` float DEFAULT '0',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
+  `updated_at` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `waiterdeposits` (
-  `id` int(11) NOT NULL,
-  `waiter_id` int(11) DEFAULT NULL,
-  `amount` float DEFAULT '0',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `withdrawals` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `admin_id` int(11) DEFAULT '0',
   `amount` float DEFAULT '0',
   `detail` varchar(100) DEFAULT '',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
+  `updated_at` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+
