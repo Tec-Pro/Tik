7b4e7d94464568257d79dd48abf4714c37cb2ee9
jheredia <joaquinheredia93@gmail.com>
2015-07-06 10:54:46 -0500

    Corrección script de tablas de depósitos

--- Sql/alterTableDepositWithdrawal.sql
+++ Sql/alterTableDepositWithdrawal.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `tik`.`deposits` 
-CHANGE COLUMN `turn` `turn` VARCHAR(45) NULL AFTER `amount`;
+ADD COLUMN `column` VARCHAR(45) NULL AFTER `amount`;
 
 ALTER TABLE `tik`.`withdrawals` 
 ADD COLUMN `turn` VARCHAR(45) NULL AFTER `detail`;
