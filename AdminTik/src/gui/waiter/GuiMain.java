e88d2427376805512555dec2814932202da06902
norcasitas <nico.orcasitas@gmail.com>
2015-07-14 23:35:12 -0300

    exit on close false, no imprimir descuentos

--- AdminTik/src/gui/waiter/GuiMain.java
+++ AdminTik/src/gui/waiter/GuiMain.java
@@ -168,7 +168,7 @@ public class GuiMain extends javax.swing.JFrame {
             .addGap(0, 100, Short.MAX_VALUE)
         );
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("SISTEMA MOZOS TIK");
 
         jScrollPane1.setBorder(null);
